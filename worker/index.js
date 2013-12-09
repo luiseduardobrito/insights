14f14b384ec1121c2e6b2044f9cd27bb3f2184e2
Luis Eduardo Brito <luiseduardo14@gmail.com>
2013-12-09 00:38:27 -0200

    painel instalado

---
+++ worker/index.js
@@ -0,0 +1,21 @@
+var WorkerHandler = function() {
+	
+	var _this = this;
+	var _public = {};
+
+	_this.workers = [];
+
+	_this.init = function() {
+		return _public;
+	}
+
+	_public.start = function(max) {
+		return null;
+	}
+
+	_public.stop = function(max) {
+		return null;
+	}
+
+	return _this.init();
+}
